14a7320c99a5fb78461837e44ef791924d4a3908
leond <lad258@cornell.edu>
2018-01-12 14:36:35 -0500

    added explicit method to grab "-M email_address" out of the arguments

--- runpipeline.sh
+++ runpipeline.sh
@@ -1,8 +1,7 @@
 if [ $# -lt 2 ]; then
-  echo Usage: $0 stage SubjectID [optarg]
-  echo "All optional arguments are passed to qsub. Type \"man qsub\" for further details."
+  echo Usage: $0 [-M email_address] stage SubjectID [optarg]
   echo Example: $0 coreg R1001P [NOREG]
-  echo Example: $0 loc R1001P [-M anon@my_email_server.com]
+  echo Example: $0 [-M anon@my_email_server.com] loc  R1001P
   echo Example: $0 norm R1001P
   echo Example: $0 walk R1001P
   exit 1
@@ -13,18 +12,29 @@ if [ -z $RAMROOT ]; then
   exit 1
 fi
 
-
 oldcwd=$PWD
 SDIR=$(dirname $0)
 cd $SDIR
 SDIR=$PWD
+
+while getopts ':M:' opts; do
+    case ${opts} in
+        M ) mail="-M ${OPTARG}"
+            shift $((OPTIND-1))
+            ;;
+        \?) ;;
+        \:);;
+    esac
+done
+OPTIND=1
+
 
 shift
 for i in $1;do
   cd $RAMROOT/${i}; 
   rm ${stg}.log
   # qsub -P RAM_DCC -q matlab.q -l h_vmem=30.1G,s_vmem=30G -cwd -j y -o ${stg}.log -V -N ${stg}$i $SDIR/${stg}.sh $*; 
-  qsub  -q matlab.q -l h_vmem=30.1G,s_vmem=30G -cwd -j y -m ea -o ${stg}.log -V -N ${stg}$i $SDIR/${stg}.sh "$@";
+  qsub  -q matlab.q -l h_vmem=30.1G,s_vmem=30G -cwd -j y -m ea ${mail} -o ${stg}.log -V -N ${stg}$i $SDIR/${stg}.sh "$@";
   # no DCC #  qsub -l h_vmem=20.1G,s_vmem=20G -cwd -j y -o ${stg}.log -V -N ${stg}$i $SDIR/${stg}.sh $*; 
 done
 
