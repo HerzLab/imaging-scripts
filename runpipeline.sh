f05d2acd0c692ef1017869392ba3ff46f6824be7
leond <lad258@cornell.edu>
2018-01-17 15:45:07 -0500

    mtlseg and runpipeline are back to using my email notification hack, rather than relying on qsub

--- runpipeline.sh
+++ runpipeline.sh
@@ -1,3 +1,16 @@
+#! /bin/bash
+
+mail_result() {
+    if [ #? == 0 ] ;then
+      result='was successful'
+    else
+       result='failed'
+    fi
+    message="Whole-brain segmentation for subject ${1} ${result}"
+    echo $message | mail -s "Whole-brain segmentation" -r ${USER}@rhino2.psych.upenn.edu ${2}
+}
+
+
 if [ $# -lt 2 ]; then
   echo Usage: $0 [-M email_address] stage SubjectID [optarg]
   echo Example: $0 coreg R1001P [NOREG]
@@ -19,8 +32,9 @@ SDIR=$PWD
 
 while getopts ':M:' opts; do
     case ${opts} in
-        M ) mail="-M ${OPTARG}"
+        M )
             shift $((OPTIND-1))
+            mail_str="mail_result "${1}_${2}" ${OPTARG}"
             ;;
         \?) ;;
         \:);;
@@ -34,7 +48,8 @@ for i in $1;do
   cd $RAMROOT/${i}; 
   rm ${stg}.log
   # qsub -P RAM_DCC -q matlab.q -l h_vmem=30.1G,s_vmem=30G -cwd -j y -o ${stg}.log -V -N ${stg}$i $SDIR/${stg}.sh $*; 
-  qsub  -q matlab.q -l h_vmem=30.1G,s_vmem=30G -cwd -j y -m ea ${mail} -o ${stg}.log -V -N ${stg}$i $SDIR/${stg}.sh "$@";
+  { qsub  -q matlab.q -l h_vmem=30.1G,s_vmem=30G -cwd -sync y -j y -o ${stg}.log -V -N ${stg}$i $SDIR/${stg}.sh "$@";
+    eval ${mail_str};} &
   # no DCC #  qsub -l h_vmem=20.1G,s_vmem=20G -cwd -j y -o ${stg}.log -V -N ${stg}$i $SDIR/${stg}.sh $*; 
 done
 
