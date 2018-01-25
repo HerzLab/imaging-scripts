a0f2dd87a37577fd4cc3fef451838e344a40e108
leond <lad258@cornell.edu>
2018-01-25 12:29:00 -0500

    option parsing now happens before anything else in runpipeline.sh

--- runpipeline.sh
+++ runpipeline.sh
@@ -19,17 +19,10 @@ if [ $# -lt 2 ]; then
   echo Example: $0 walk R1001P
   exit 1
 fi
-stg=$1
 if [ -z $RAMROOT ]; then
   echo "Please define RAMROOT to point to the top level image analysis directory"
   exit 1
 fi
-
-oldcwd=$PWD
-SDIR=$(dirname $0)
-cd $SDIR
-SDIR=$PWD
-
 while getopts ':M:' opts; do
     case ${opts} in
         M )
@@ -40,16 +33,23 @@ while getopts ':M:' opts; do
         \:);;
     esac
 done
+
+oldcwd=$PWD
+SDIR=$(dirname $0)
+cd $SDIR
+SDIR=$PWD
+
 OPTIND=1
 
+stg=$1
 
 shift
 for i in $1;do
   cd $RAMROOT/${i}; 
-  rm ${stg}.log
+  #rm ${stg}.log
   # qsub -P RAM_DCC -q matlab.q -l h_vmem=30.1G,s_vmem=30G -cwd -j y -o ${stg}.log -V -N ${stg}$i $SDIR/${stg}.sh $*; 
   { qsub  -q matlab.q -l h_vmem=30.1G,s_vmem=30G -cwd -sync y -j y -o ${stg}.log -V -N ${stg}$i $SDIR/${stg}.sh "$@";
-    eval ${mail_str};} &
+    eval ${mail_str}; } &
   # no DCC #  qsub -l h_vmem=20.1G,s_vmem=20G -cwd -j y -o ${stg}.log -V -N ${stg}$i $SDIR/${stg}.sh $*; 
 done
 
