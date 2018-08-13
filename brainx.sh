1d140cf33b9bf0944b1b08801cd7d26e0c6f1602
leond <lad258@cornell.edu>
2018-08-13 17:30:40 -0400

    Added brainx.sh to repo
    (cherry picked from commit 384b7aa)

---
+++ brainx.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#$ -S /bin/bash
+
+
+sub=$1
+FILE=$PWD/T00_${sub}_mprage.nii.gz
+OUT_DIR=$PWD
+TEMPLATE_DIR=/home1/dorian.pustina/pennTemplate
+export ANTSPATH=~sudas/bin/localization/ants_avants_Dec162013/
+
+  ${ANTSPATH}antsBrainExtraction.sh \
+            -d 3 \
+            -a $FILE \
+            -f ${TEMPLATE_DIR}/templateBrainExtractionRegistrationMask.nii.gz \
+            -e ${TEMPLATE_DIR}/template.nii.gz \
+            -m ${TEMPLATE_DIR}/templateBrainMask.nii.gz \
+            -o $OUT_DIR/T00_${sub}_mprage_brain
+
+
